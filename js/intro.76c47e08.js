d773e97e4155e8781d3b36d8f19cc69c4b1352a1
Fernanda Zuluaga <mariafer2007gz@gmail.com>
2026-04-25 02:07:00 +0000

    hola

---
+++ js/intro.76c47e08.js
@@ -0,0 +1,89 @@
+/* =============================================================
+   COMPONENTE: INTRODUCCIÓN (intro.js)
+   ============================================================= */
+
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([
+  ["intro"],
+  {
+    "0b16": function(t, e, a) {
+      "use strict";
+      a("8b10");
+    },
+    "5167": function(t, e, a) {
+      "use strict";
+      a.r(e);
+
+      // 1. RENDERIZADO DEL CONTENEDOR
+      var i = function() {
+        var t = this,
+          e = t.$createElement,
+          a = t._self._c || e;
+        return a(
+          "div",
+          { staticClass: "curso-main-container introduccion" },
+          [a("BannerInterno"), t._m(0)],
+          1
+        );
+      };
+
+      // 2. CONTENIDO ESTÁTICO (Título, Texto y Video)
+      var n = [
+        function() {
+          var t = this,
+            e = t.$createElement,
+            a = t._self._c || e;
+          return a(
+            "div",
+            { staticClass: "container tarjeta tarjeta--blanca p-4 p-md-5 mb-5" },
+            [
+              // Bloque del Título
+              a("div", { staticClass: "titulo-principal", attrs: { "data-aos": "flip-up" } }, [
+                a("div", { staticClass: "titulo-principal__numero" }, [
+                  a("span", [a("i", { staticClass: "fas fa-info" })])
+                ]),
+                a("h1", [t._v("Introducción")])
+              ]),
+
+              // Párrafo de introducción
+              a("p", { staticClass: "mb-5" }, [
+                t._v("A continuación, se presentará el video que hará una breve introducción del presente componente formativo:")
+              ]),
+
+              // Contenedor del Video (iframe)
+              a("figure", [
+                a("div", { staticClass: "video" }, [
+                  a("iframe", {
+                    attrs: {
+                      width: "560",
+                      height: "315",
+                      src: "https://www.youtube.com/embed/h5U98IQxP4E",
+                      title: "YouTube video player",
+                      frameborder: "0",
+                      allow: "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",
+                      allowfullscreen: ""
+                    }
+                  })
+                ])
+              ])
+            ]
+          );
+        }
+      ];
+
+      // 3. CONFIGURACIÓN DEL COMPONENTE VUE
+      var r = {
+        name: "Introduccion",
+        data: function() {
+          return {};
+        }
+      };
+
+      var c = r,
+        o = (a("0b16"), a("2877")),
+        s = Object(o["a"])(c, i, n, !1, null, "14b78173", null);
+
+      e["default"] = s.exports;
+    },
+    "8b10": function(t, e, a) {}
+  }
+]);
